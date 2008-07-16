fdbe4d5be8b89ad0401730df54b7692ceb2a1637
Greg Miller <devjgm@hidden>
2008-07-16 20:06:25 +0000

    First commit of the Keystone-based MacFUSE autoinstaller
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@1055 febdc8c4-1326-0410-abd6-27cb15717650

---
+++ core/autoinstaller/externals/GTM/UnitTesting/RunIPhoneUnitTest.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#  RunIPhoneUnitTest.sh
+#  Copyright 2008 Google Inc.
+#  
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License.  You may obtain a copy
+#  of the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+#
+#  Runs all unittests through the iPhone simulator. We don't handle running them
+#  on the device. To run on the device just choose "run".
+
+if [ "$IPHONEOS_DEPLOYMENT_TARGET" == "" ]; then
+  export DYLD_ROOT_PATH="$SDKROOT"
+  export DYLD_FRAMEWORK_PATH="$CONFIGURATION_BUILD_DIR"
+  export IPHONE_SIMULATOR_ROOT="$SDKROOT"
+  export CFFIXED_USER_HOME="$USER_LIBRARY_DIR/Application Support/iPhone Simulator/User"
+  "$TARGET_BUILD_DIR/$EXECUTABLE_PATH" -RegisterForSystemEvents
+else
+  echo "note: Skipping running of unittests for device build."
+fi
+exit 0
